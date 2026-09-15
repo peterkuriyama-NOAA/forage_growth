692606cb6cb13feb5cd9dbbd03bdf129216f670a
peter.kuriyama@noaa <peter.kuriyama@noaa.gov>
2026-09-15 14:17:16 -0700

    add processing of results

--- run1.R
+++ run1.R
@@ -110,7 +110,55 @@ print(end_time)
 #4 hours for 100 iterations
 
 
+#----------------------------------------------------------------------------
+#------------------------------------------------------
+#Read in Results
+iters <- 1:100
+#Data scenarios that focus on index sampling
+folds <- list.files("results")[grep(paste0("D", c(11:13, 21:23), collapse ="|"), list.files("results"))]
+folds <- paste0("results/", folds)
 
+folds <- folds[grep("EM11|EM12|EM13|EM14", folds)]
+
+mods <- c("om", "em")
+flz <- expand_grid(folds, iters, mods) %>% mutate(unq = paste(folds,
+                                                              iters, mods, sep = '/')) %>%
+  pull(unq)
+
+
+ncores <- 100
+
+cl <- makeCluster(ncores)
+registerDoParallel(cl)
+
+start_time <- Sys.time()
+reslist <- foreach::foreach(ii = flz, .packages = "r4ss") %dopar%
+  SS_output(ii, printstats = F, covar = F)
+stopCluster(cl)
+end_time <- Sys.time() - start_time; end_time
+
+#100 iterations, 36 scenarios run took 3.4 minutes
+
+names(reslist) <- flz
+save(reslist, file = "results/D11toD23_EM11toEM14_reslist.Rdata")
+
+#------------------------------------------------------
+#Process the results
+tsres <- pull_timeseries(reslist)
+
+save(tsres, file = "results/D11toD23_EM11toEM14_tsres.Rdata")
+
+tsRE <- calc_re(tsres, colname = "Bio_smry")
+
+
+# datdesc <- data.frame(datscen = paste0("D", 31:36), 
+#                       datdesc  =paste0("D", 31:36, "_F1_", 
+#                                        datscen$sl.Nsamp.1, "_F2_", datscen$sl.Nsamp.2))
+# datdesc <- datdesc %>% distinct(datscen, datdesc)
+# tsRE <- tsRE %>% left_join(datdesc, by = 'datscen')  
+
+#Save the results
+save(tsRE, file = "results/D11toD23_EM11toEM14_tsRE.Rdata")
 
 
 
@@ -192,14 +240,10 @@ print(end_time)
 
 #-------------------------------------
 #Results
-iters <- 1:150
+iters <- 1:100
 #Data scenarios that focus on index sampling
 folds <- list.files("results")[grep(paste0("D", 31:36, collapse ="|"), list.files("results"))]
 folds <- paste0("results/", folds)
-
-# folds <- folds[grep("EM4", folds)]
-# folds <- folds[grep("EM2", folds)]
-
 
 mods <- c("om", "em")
 flz <- expand_grid(folds, iters, mods) %>% mutate(unq = paste(folds,
@@ -218,9 +262,11 @@ reslist <- foreach::foreach(ii = flz, .packages = "r4ss") %dopar%
 stopCluster(cl)
 end_time <- Sys.time() - start_time; end_time
 
+#100 iterations, 36 scenarios run took 6.4 minutes
+
 names(reslist) <- flz
 
-#Took 4 minutes to read in 150 iters
+
 
 # modnames <- data.frame(model = flz)
 # mm <- strsplit(modnames$model, split = "/") %>% ldply
@@ -235,6 +281,7 @@ names(reslist) <- flz
 #-------------------------------------
 ###Plot results
 tsres <- pull_timeseries(reslist)
+
 # tsres <- tsres %>% left_join(modnames)
 
 tsRE <- calc_re(tsres, colname = "Bio_smry")
--- run2.R
+++ run2.R
@@ -1,10 +1,11 @@
+#------------------------------------------------------
 ####
-#Run 2
+#Run 2; Dat Scens
 
 dat_scens <- read.csv("models/dat_scens.csv")
 dat_scens$X <- NULL
 
-
+#------------------------------------------------------
 #Run models
 iters <- 1:100
 ncores <- 100
@@ -17,3 +18,119 @@ scname <- run_ss3sim(iterations = iters, simdf = dat_scens, parallel = T,
 stopCluster(cl)
 end_time <- Sys.time() - start_time
 print(end_time) 
+
+
+dat_scens
+
+#------------------------------------------------------
+#Read in Results
+iters <- 1:100
+#Data scenarios that focus on index sampling
+folds <- list.files("results")[grep(paste0("D", 31:36, collapse ="|"), list.files("results"))]
+folds <- paste0("results/", folds)
+
+folds <- folds[grep("EM11|EM12|EM13|EM14", folds)]
+
+mods <- c("om", "em")
+flz <- expand_grid(folds, iters, mods) %>% mutate(unq = paste(folds,
+                                                              iters, mods, sep = '/')) %>%
+  pull(unq)
+
+
+ncores <- 100
+
+cl <- makeCluster(ncores)
+registerDoParallel(cl)
+
+start_time <- Sys.time()
+reslist <- foreach::foreach(ii = flz, .packages = "r4ss") %dopar%
+  SS_output(ii, printstats = F, covar = F)
+stopCluster(cl)
+end_time <- Sys.time() - start_time; end_time
+
+#100 iterations, 36 scenarios run took 6.4 minutes
+
+names(reslist) <- flz
+save(reslist, file = "results/D31toD36_EM11toEM14_reslist.Rdata")
+
+#------------------------------------------------------
+#Process the results
+tsres <- pull_timeseries(reslist)
+
+save(tsres, file = "results/D31toD36_EM11toEM14_tsres.Rdata")
+
+tsRE <- calc_re(tsres, colname = "Bio_smry")
+
+
+datdesc <- data.frame(datscen = paste0("D", 31:36), 
+                      datdesc  =paste0("D", 31:36, "_F1_", 
+                                       datscen$sl.Nsamp.1, "_F2_", datscen$sl.Nsamp.2))
+datdesc <- datdesc %>% distinct(datscen, datdesc)
+tsRE <- tsRE %>% left_join(datdesc, by = 'datscen')  
+
+#Save the results
+save(tsRE, file = "results/D31toD36_EM11toEM14_tsRE.Rdata")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+# tsRE %>% ggplot(aes(x = Yr, y = re, group = iter)) + geom_line() + geom_hline(yintercept = 0,  col = 'red') + 
+#   facet_grid(estmod ~ datdesc)
+# 
+# # dev.size()
+# # ggsave("figs/D31_D36_comp_effects.png", width = 11.1, height = 4)
+# 
+# 
+# 
+# ###Median values
+# tsREsumm <- tsRE %>% group_by(Yr, scen, datscen, datdesc, estmod) %>% summarize(lo10 = quantile(re, .1), mid = median(re),
+#                                                                                 hi90 = quantile(re, .9)) 
+# 
+# 
+# tsREsumm %>%
+#   ggplot(aes(x = Yr)) + geom_line(aes(y = lo10), lty = 2) + geom_line(aes(y = hi90), lty = 2) + 
+#   geom_line(aes(y = mid)) + facet_grid(datdesc ~ estmod) + 
+#   geom_hline(yintercept = 0,  col = "red") + ylab("Relative Error") + xlab("Year")
+# 
+# 
+# tsREsumm %>%
+#   ggplot(aes(x = Yr)) + 
+#   geom_line(aes(y = mid, group = estmod, color = estmod)) +
+#   geom_hline(yintercept = 0, lty = 2, col = "red") + ylab("Relative Error") + xlab("Year") + 
+#   facet_wrap(~ datdesc) 
+# 
+# 
+# 
+# 
